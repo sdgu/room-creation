b0bb5fb8960f934183c734465efe67cb01fe2add
Shawn <sdgu000@gmail.com>
2016-03-20 20:14:01 -0400

    for creating rooms with angular router

---
+++ public/javascripts/ff.js
@@ -0,0 +1,111 @@
+var app = angular.module("ffcards", ["ui.router", "ngSanitize"]);
+
+
+app.factory("createRoom", function($http, $window)
+{
+
+	var data = 
+	{
+		room: []
+	}
+
+	data.create = function(roomID)
+	{
+		return $http.post("/createroom", roomID, {}).success(function(data)
+		{
+			
+		})
+	}
+
+
+	return data;
+
+
+});
+
+app.factory("rooms", function($http)
+{
+	var o =
+	{
+		rooms: []
+	}
+
+
+	o.getByID = function(id)
+	{
+		return $http.get("/restful/rooms/" + id).then(function(res)
+		{
+			return res.data;
+		})
+	}
+
+	o.getAll = function()
+	{
+		return $http.get("/restful/rooms").success(function(data)
+		{
+			angular.copy(data, o.rooms);
+		})
+	}
+
+	return o;
+})
+
+app.controller("MainCtrl", function($scope, createRoom)
+{
+
+	
+
+
+	$scope.createRoom = function()
+	{
+		var randID = Math.random().toString(36).slice(2);
+		var date = new Date();
+		$scope.roomID = randID;
+		createRoom.create({_id: randID, dateCreated: date});
+	}
+
+});
+
+
+app.controller("RoomCtrl", function($scope, rooms, post)
+{
+
+
+	$scope.roomID = post._id;
+
+})
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider)
+{
+	$urlRouterProvider.otherwise("/");
+
+	$stateProvider.state("main",
+	{
+		url: "/",
+		templateUrl: "/views/main.html",
+		controller: "MainCtrl as m"
+		// resolve:
+  //     	{
+  //       	postPromise: ["poems", function(poems)
+  //       	{
+  //         		return poems.getAll();
+  //       	}]
+  //     	}
+	});
+
+	$stateProvider.state("room",
+	{
+		url: "/{roomID}",
+		templateUrl: "/views/room.html",
+		controller: "RoomCtrl as r",
+		resolve:
+		{
+			post: ["$stateParams", "rooms", function($stateParams, rooms)
+			{
+				
+				return rooms.getByID($stateParams.roomID);
+			}]
+		}
+	})
+
+});
